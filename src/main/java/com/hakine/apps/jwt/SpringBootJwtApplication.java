0049717fed235b0461e2f1237e115aa308c1328e
rockoil <rockoil@gmail.com>
2019-06-21 15:59:34 +0900

    초기 커밋

---
+++ src/main/java/com/hakine/apps/jwt/SpringBootJwtApplication.java
@@ -0,0 +1,7 @@
+package com.hakine.apps.jwt;
+
+public class SpringBootJwtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtApplication.class, args);
+    }
+}
